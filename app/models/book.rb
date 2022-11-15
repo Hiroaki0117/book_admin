7da231e35c785cacd4501e8cee292f4e17d04801
hiroaki0117 <nishimura.hn.0117@gmail.com>
2022-11-15 10:39:43 +0900

    Initialize Comit

---
+++ app/models/book.rb
@@ -0,0 +1,27 @@
+class Book < ApplicationRecord
+    scope :costly, -> {where("price > ?", 3000)}
+    scope :wrtten_about, ->(theme) {where("name like ?", "%#{theme}%")}
+
+    belongs_to :publisher
+    has_many :book_authors
+    has_many :authors, through: :book_authors
+
+    validates :name, presence: true
+    validates :name, length:{maximum: 25}
+    validates :price, numericality: {greater_than_or_equal_to: 0}
+
+    before_validation do
+        self.name = self.name.gsub(/Cat/) do |matched|
+            "lovely #{matched}"
+        end
+    end
+
+    after_destroy :if => :high_price? do
+        Rails.logger.warn "Book is high price Deleted: #{self.attributes}"
+        Rails.logger.warn "Please Cheack"
+    end
+
+    def high_price?
+        price >= 5000
+    end
+end
